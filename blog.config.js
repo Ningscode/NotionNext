61e8fe4c0e789988cd5ceb4c73738eb53d6df2ff
Ning <77867541+Ningscode@users.noreply.github.com>
2024-11-02 10:28:34 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,7 +24,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '回到未來', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '思想，是迷航中的燈塔', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.reasoning.top', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '英语,学习,同文,博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '学习,博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'Ningsblog@outlook.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
